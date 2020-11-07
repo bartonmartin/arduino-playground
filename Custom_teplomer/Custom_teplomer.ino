bee1886cb4b1cc0c7b4e37e114f16fb305b0b1f7
bartonmartin <bartonmartin@gmail.com>
2020-11-07 16:08:32 +0100

    Remove not used code

--- Custom_teplomer/Custom_teplomer.ino
+++ Custom_teplomer/Custom_teplomer.ino
@@ -1,9 +1,5 @@
 #include <WEMOS_SHT3X.h>
 
-
-#define PIN_D1 D1
-#define PIN_D2 D2
-
 SHT3X sht30(0x45);
 
 void setup() {
@@ -13,11 +9,6 @@ void setup() {
 }
 
 void loop() {
-
-
-  Serial.println(String("pin 1: " )+ String(D1));
-  Serial.println(String("pin 2: " )+ String(D2));
-  
   if(sht30.get()==0){
     Serial.print("Temperature in Celsius : ");
     Serial.println(sht30.cTemp);
